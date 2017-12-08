3acfcbe1c9f58e970a74f9b8dad2414e754c54ac
Levy Medina II <levymedina3@gmail.com>
2017-12-08 20:27:09 +0800

    initial commit, server test

---
+++ server.py
@@ -0,0 +1,23 @@
+import json
+import falcon
+import requests
+
+class WelcomeResource(object):
+    def on_get(self, req, res):
+        #Test route
+        root = 'https://reqres.in'
+        route = '/api/users?page=2'
+        
+        test_data = requests.get(root + route)
+        res.status = falcon.HTTP_200  # This is the default status
+        test_data = test_data.json()
+        res.body = json.dumps(test_data)
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+welcome = WelcomeResource()
+
+# things will handle all requests to the '/things' URL path
+app.add_route('/welcome', welcome)
