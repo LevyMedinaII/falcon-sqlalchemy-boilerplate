b3554a2dbb8f4a8d265b2813784ccb51d1c5bf5d
Levy Medina II <levymedina3@gmail.com>
2017-12-08 20:36:50 +0800

    changed route name

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ import json
 import falcon
 import requests
 
-class WelcomeResource(object):
+class TestResource(object):
     def on_get(self, req, res):
         #Test route
         root = 'https://reqres.in'
         route = '/api/users?page=2'
-        
+
         test_data = requests.get(root + route)
         res.status = falcon.HTTP_200  # This is the default status
         test_data = test_data.json()
@@ -17,7 +17,7 @@ class WelcomeResource(object):
 app = falcon.API()
 
 # Resources are represented by long-lived class instances
-welcome = WelcomeResource()
+test = TestResource()
 
 # things will handle all requests to the '/things' URL path
-app.add_route('/welcome', welcome)
+app.add_route('/test', test)
